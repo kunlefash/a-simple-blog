0649b6c58d5006d94b057b00863d64a2f4116e66
kunlefash <kunlefashmayop@gmail.com>
2022-02-27 06:19:57 -0400

    main

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
-
+const bcrypt = require("bcrypt")
 //update user
 // allows the use any userId
 router.put("/:id" , async (req, res) => {
