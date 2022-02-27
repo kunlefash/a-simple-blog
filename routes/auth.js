dd752594a8ccc798cb62728760ad946fde658868
kunlefash <kunlefashmayop@gmail.com>
2022-02-27 06:08:24 -0400

    main

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 //Register
 router.post("/register", async (req, res) => {
@@ -13,10 +13,25 @@ router.post("/register", async (req, res) => {
         res.status(200).json(user);
 
     }catch(err){
-        console.log(err)
+        res.status(500).json(err)
     }
 
     
 });
+//LOGIN
+router.post("/login", async (req, res) => {
+    try{   
+        // Check if the user exists
+        const user = await User.findOne({email:req.body});
+        !user && res.status(404).json("User is not found")
+        // Compares to confirm if the password inputed is correct
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("Wrong password");
+        // returns user if the user exists
+        res.status(200).json(user);
+    } catch(err)  {
+    res.status(500).json(err)
+    }
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,35 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-    res.send("It is the user")
-});
+//update user
+// allows the use any userId
+router.put("/:id" , async (req, res) => {
+    //Verify if the user ID matches with the particular ID and also checkd if the user is an admin
+    if(req.body.userId === req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            //
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt)
+            } catch(err){
+                return res.status(500).json(err);
 
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id.at, {$set: req.body,});
+
+            res.status(200).json("account has beem updated")
+        } catch(err) {
+            return res.json(500).json(err);
+        }
+
+    }else {
+        return res.status(403).json("You can only update your account")
+    }
+})
+//delete user
+//get a user
+//follow a user
 module.exports = router;
