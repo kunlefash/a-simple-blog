bd694bc3451f7d631bb552dafbff2508be11cc95
kunlefash <kunlefashmayop@gmail.com>
2022-03-02 08:58:10 -0400

    main

--- routes/post.js
+++ routes/post.js
@@ -1,4 +1,5 @@
 const res = require("express/lib/response");
+const { reset } = require("nodemon/lib/utils");
 const Post = require("../models/Post")
 const router = require("express").Router();
 
@@ -59,6 +60,51 @@ router.delete("/:id", async (req, res) =>{
     
 })
 
-//Like post
+//Like and Dislike  post
+
+router.put("/:id/like", async (req, res)=> {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push:{likes:req.body.userId}});
+            res.status(200).json("liked")
+        } else{
+            await post.updateOne({$pull:{likes:req.body,userId}});
+            res.status(200).json("Disliked");
+        }
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
 //Get post
+
+router.get("/:id", async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+//get timeline
+
+router.get("/all", async (req, res) =>{
+   try{
+        const currentUser = await User.findById(reqe.body.userId);
+        const userPost = await Post.find({userId: currentUser._id});
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId)=> {
+                return Post.find({userId: friendId})
+            })
+        );
+        res.json(userPost.concat(...friendPosts))
+   }catch(err){
+       res.status(500).json(err)
+   }
+})
 module.exports = router;
